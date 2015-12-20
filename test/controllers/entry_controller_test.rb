9afe3ffeda51e744e19d4febc37842fca79857ba
Neyanbhbin <yongjie.kanng@gmail.com>
2015-12-19 23:53:33 -0600

    readd entry controller

--- test/controllers/entry_controller_test.rb
+++ test/controllers/entry_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class EntriesControllerTest < ActionController::TestCase
+class EntryControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
