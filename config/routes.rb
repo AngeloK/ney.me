09ff44d4eabb0b55e6d73d276342135ba6e34f78
Neyanbhbin <kanng.yongjie@gmail.com>
2015-12-23 01:02:04 -0600

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Make sure this routeset is defined last
 
   # The priority is based upon order of creation: first created -> highest priority.
